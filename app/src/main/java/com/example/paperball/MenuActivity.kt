37d61813fa2da08cf346dab4002d0cbc4287872e
idunsky855 <idunsky855@gmail.com>
2024-09-30 17:09:57 +0300

    added menu activity - unfinished

---
+++ app/src/main/java/com/example/paperball/MenuActivity.kt
@@ -0,0 +1,43 @@
+package com.example.paperball
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var menu_BTN_buttons: MaterialButton
+    private lateinit var menu_BTN_sensors: MaterialButton
+    private lateinit var menu_BTN_scores: MaterialButton
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        findViews()
+        initViews()
+    }
+
+    private fun initViews() {
+        menu_BTN_buttons.setOnClickListener {  }
+
+        menu_BTN_sensors.setOnClickListener {  }
+
+        menu_BTN_scores.setOnClickListener {  }
+    }
+
+    private fun findViews() {
+        menu_BTN_buttons = findViewById(R.id.menu_BTN_buttons)
+        menu_BTN_sensors = findViewById(R.id.menu_BTN_sensors)
+        menu_BTN_scores = findViewById(R.id.menu_BTN_scores)
+    }
+
+    private fun switchActivity(){
+
+    }
+}
